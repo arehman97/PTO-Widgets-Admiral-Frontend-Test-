ce061a57654a47ec9c7ff67e5ddff404f5f4c6c7
Abdul Rehman <arkhan7991@gmail.com>
2024-06-26 12:12:19 +0500

    src

--- src/components/Pto-Leaderboard/Pto-Leaderboard.js
+++ src/components/Pto-Leaderboard/Pto-Leaderboard.js
@@ -4,6 +4,7 @@ export default {
   data() {
     return {
       gender: 'men',
+      loading: true,
       leaderboard: {
         men: [],
         women: []
@@ -15,14 +16,33 @@ export default {
   },
   methods: {
     async fetchLeaderboard() {
+      this.loading = true;
       try {
         const menStatsResponse = await axios.get('https://rankings.test.protriathletes.org/api/t100/season/2024/standings/MPRO');
         const womenStatsResponse = await axios.get('https://rankings.test.protriathletes.org/api/t100/season/2024/standings/FPRO');
-        this.leaderboard.men = menStatsResponse?.data.men;
-        this.leaderboard.women = womenStatsResponse?.data.women;
+        this.leaderboard.men = this.parseAthleteData(menStatsResponse?.data?.rankings?.filter(athletes => athletes?.rank <= 5));
+        this.leaderboard.women = this.parseAthleteData(womenStatsResponse?.data?.rankings?.filter(athletes => athletes?.rank <= 5));
       } catch (error) {
         console.error('Error fetching leaderboard:', error);
+      } finally {
+        this.loading = false;
       }
+    },
+    parseAthleteData(athletes) {
+      return athletes.map(athlete => ({
+        first: athlete.first,
+        last: athlete.last,
+        rank: athlete.rank,
+        points: athlete.points,
+        imageUrl: this.getAthleteImage(athlete?.pic)
+      }));
+    },
+    getAthleteImage(pic) {
+      let parsedPicture = ''
+      if (pic !== null) {
+        parsedPicture = JSON.parse(pic)
+      }
+      return parsedPicture?.img?.src; 
     }
   }
 };
--- src/components/Pto-Leaderboard/Pto-Leaderboard.vue
+++ src/components/Pto-Leaderboard/Pto-Leaderboard.vue
@@ -1,41 +1,54 @@
 <template>
   <p class="text-uppercase font-transducer pt-3 m-0">Leaderboard</p>
   <span class="text-uppercase font-transducer font-heading">Race to the top</span>
-  <div class="row mt-2">
+  <div v-if="loading" class="text-center mt-4">
+    <div class="spinner-border" role="status">
+      <span class="sr-only"></span>
+    </div>
+  </div>
+  <div v-else class="row mt-4">
     <div class="col-md-2 col-sm-12 col-xs-12">
       <p class="font-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
     <div class="col-md-5 col-sm-12 col-xs-12 mb-4">
       <p class="text-uppercase font-transducer">Men</p>
-      <div class="card">
-        <div class="card-header">
-          <ul class="nav nav-tabs card-header-tabs">
-            <li class="nav-item">
-              <a class="nav-link" :class="{ active: gender === 'men' }" @click="gender = 'men'">Men</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" :class="{ active: gender === 'women' }" @click="gender = 'women'">Women</a>
-            </li>
-          </ul>
+      <div class="position-relative mb-4" v-for="player in leaderboard['men']" :key="player.id">
+        <div class='small-slanted'>
+          <p class="font-transducer"><i>#</i>{{player?.rank}}</p>
         </div>
-        <div class="card-body">
-          <ul class="list-group">
-            <li class="list-group-item" v-for="player in leaderboard[gender]" :key="player.id">
-              {{ player.name }} - {{ player.points }} pts
-            </li>
-          </ul>
+        <div class="background">
+          <img src="../../../public/backgroundImages/bg-img.png" alt="Athlete Image" loading="lazy"/>
+        </div>
+        <div class="bg-image">
+          <img :src="player.imageUrl" alt="Athlete Image" loading="lazy"/>
+        </div>
+        <div class='large-slanted d-flex'>
+          <div class="name position-relative">            
+            <div class="flagImage"><img src="../../../public/backgroundImages/denmarkFlag.png" alt="Flag Image" loading="lazy"/></div>
+            <p class="font-transducer mb-0 font-responsive"><span>{{player?.first}}</span><br />{{player?.last}}</p>
+          </div>
+          <div class="font-transducer points"><p class="mb-0">{{player?.points}} <br /><span>PTS</span></p></div>
         </div>
       </div>
     </div>
     <div class="col-md-5 col-sm-12 col-xs-12">
       <p class="text-uppercase font-transducer">Women</p>
-      <div class="position-relative">
+      <div class="position-relative mb-4" v-for="player in leaderboard['women']" :key="player.id">
         <div class='small-slanted'>
-          <p class="font-transducer"><i>#</i>1</p>
+          <p class="font-transducer"><i>#</i>{{player?.rank}}</p>
+        </div>
+        <div class="background">
+          <img src="../../../public/backgroundImages/bg-img.png" alt="Athlete Image" loading="lazy"/>
+        </div>
+        <div class="bg-image">
+          <img :src="player.imageUrl" alt="Athlete Image" loading="lazy"/>
         </div>
         <div class='large-slanted d-flex'>
-          <p class="font-transducer mb-0 name"><span>Lucy</span><br />Charles-Barclay</p>
-          <div class="font-transducer points"><p class="mb-0">50 <br/><span>PTS</span></p></div>
+          <div class="name position-relative">            
+            <div class="flagImage"><img src="../../../public/backgroundImages/denmarkFlag.png" alt="Flag Image" loading="lazy"/></div>
+            <p class="font-transducer mb-0 font-responsive"><span>{{player?.first}}</span><br />{{player?.last}}</p>
+          </div>
+          <div class="font-transducer points"><p class="mb-0">{{player?.points}} <br /><span>PTS</span></p></div>
         </div>
       </div>
     </div>
